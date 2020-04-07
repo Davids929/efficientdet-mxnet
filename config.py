9dbdb00c9ced779ecd572d96b049fbb4c172025c
shi wei <weishi@wei-Mac.local>
2020-04-07 19:44:51 +0800

    update

---
+++ config.py
@@ -0,0 +1,57 @@
+#coding=utf-8
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train EfficientDet networks.')
+    parser.add_argument('--network', type=str, default='efficientdet-b1',
+                        help="The name of detect network.")
+    parser.add_argument('--data-shape', type=int, default=640,
+                        help="Input data shape, use 300, 512.")
+    parser.add_argument('--batch-size', type=int, default=16,
+                        help='Training mini-batch size')
+    parser.add_argument('--dataset', type=str, default='coco',
+                        help='Training dataset. Now support voc.')
+    parser.add_argument('--dataset-root', type=str, default='~/.mxnet/datasets/',
+                        help='Path of the directory where the dataset is located.')
+    parser.add_argument('--num-workers', '-j', dest='num_workers', type=int,
+                        default=4, help='Number of data workers, you can use larger '
+                        'number to accelerate data loading, if you CPU and GPUs are powerful.')
+    parser.add_argument('--gpus', type=str, default='0',
+                        help='Training with GPUs, you can specify 1,3 for example.')
+    parser.add_argument('--epochs', type=int, default=240,
+                        help='Training epochs.')
+    parser.add_argument('--resume', type=str, default='',
+                        help='Resume from previously saved parameters if not None. '
+                        'For example, you can resume from ./ssd_xxx_0123.params')
+    parser.add_argument('--start-epoch', type=int, default=0,
+                        help='Starting epoch for resuming, default is 0 for new training.'
+                        'You can specify it to 100 for example to start from 100 epoch.')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='Learning rate, default is 0.001')
+    parser.add_argument('--lr-decay', type=float, default=0.1,
+                        help='decay rate of learning rate. default is 0.1.')
+    parser.add_argument('--lr-decay-epoch', type=str, default='160,200',
+                        help='epochs at which learning rate decays. default is 160,200.')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='SGD momentum, default is 0.9')
+    parser.add_argument('--wd', type=float, default=0.00004,
+                        help='Weight decay, default is 5e-4')
+    parser.add_argument('--log-interval', type=int, default=100,
+                        help='Logging mini-batch interval. Default is 100.')
+    parser.add_argument('--save-prefix', type=str, default='',
+                        help='Saving parameter prefix')
+    parser.add_argument('--save-interval', type=int, default=10,
+                        help='Saving parameters epoch interval, best model will always be saved.')
+    parser.add_argument('--val-interval', type=int, default=1,
+                        help='Epoch interval for validation, increase the number will reduce the '
+                             'training time if validation is slow.')
+    parser.add_argument('--seed', type=int, default=233,
+                        help='Random seed to be fixed.')
+    parser.add_argument('--syncbn', action='store_true',
+                        help='Use synchronize BN across devices.')
+    parser.add_argument('--amp', action='store_true',
+                        help='Use MXNet AMP for mixed precision training.')
+    
+
+    args = parser.parse_args()
+    return args
